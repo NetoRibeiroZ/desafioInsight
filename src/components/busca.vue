1bdcc35f42fd9b35c9a973a689240ad0fdf506c5
Neto Rodrigues <antoniodrigues@hotmail.com>
2023-06-20 14:17:42 -0300

    inserção das requisições e do loading

--- src/components/busca.vue
+++ src/components/busca.vue
@@ -1,48 +1,82 @@
 <template>
   <div class="container">
     <h1 class="mt-5">Busca de Obras de Arte</h1>
-  
+
     <div class="mt-5">
       <div class="row">
         <div class="col-md-4">
           <h2>Buscar por Artista:</h2>
           <div class="form-group">
             <label for="artist">Artista:</label>
-            <input type="text" id="artist" class="form-control" v-model="searchArtist">
-            <button class="btn btn-primary mt-2" @click="searchByArtist">Buscar</button>
+            <input
+              type="text"
+              id="artist"
+              class="form-control"
+              v-model="searchArtist"
+            />
+            <button class="btn btn-primary mt-2" @click="searchByArtist()">
+              Buscar
+            </button>
           </div>
         </div>
-  
+
         <div class="col-md-4">
           <h2>Buscar por Localização:</h2>
           <div class="form-group">
             <label for="location">Localização:</label>
-            <input type="text" id="location" class="form-control" v-model="searchLocation">
-            <button class="btn btn-primary mt-2" @click="searchByLocation">Buscar</button>
+            <input
+              type="text"
+              id="location"
+              class="form-control"
+              v-model="searchLocation"
+            />
+            <button class="btn btn-primary mt-2" @click="searchByLocation()">
+              Buscar
+            </button>
           </div>
         </div>
-  
+
         <div class="col-md-4">
           <h2>Buscar por Título:</h2>
           <div class="form-group">
             <label for="title">Título:</label>
-            <input type="text" id="title" class="form-control" v-model="searchTitle">
-            <button class="btn btn-primary mt-2" @click="searchByTitle">Buscar</button>
+            <input
+              type="text"
+              id="title"
+              class="form-control"
+              v-model="searchTitle"
+            />
+            <button class="btn btn-primary mt-2" @click="searchByTitle()">
+              Buscar
+            </button>
           </div>
         </div>
       </div>
     </div>
-  
+
     <h2 class="mt-5">Resultados da Busca:</h2>
-    <div class="row mt-3">
-      <div class="col-md-4" v-for="artwork in artworks" :key="artwork.objectID">
+    <div v-if="isLoading" class="col-md-4 d-flex align-items-center justify-content-center">
+      <div class="card">
+        <div class="card-body text-center">
+          <div class="spinner-border" role="status">
+          </div>
+        </div>
+      </div>
+    </div> 
+    <div class="row mt-3">    
+      <div class="col-md-4 mt-3" v-for="artwork in artworks" :key="artwork.objectID">
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">{{ artwork.title }}</h5>
-            <p class="card-text"><strong>Artista:</strong> {{ artwork.artistDisplayName }}</p>
-            <p class="card-text"><strong>Localização:</strong> {{ artwork.repository }}</p>
-            <p class="card-text"><strong>Descrição:</strong> {{ artwork.objectName }}</p>
-            <button class="btn btn-sm btn-primary" @click="showDetails(artwork.objectID)">Detalhes</button>
+            <p class="card-text">
+              <strong>Artista:</strong> {{ artwork.artistDisplayName }}
+            </p>
+            <p class="card-text">
+              <strong>Localização:</strong> {{ artwork.repository }}
+            </p>
+            <p class="card-text">
+              <strong>Descrição:</strong> {{ artwork.objectName }}
+            </p>
           </div>
         </div>
       </div>
@@ -51,41 +85,101 @@
 </template>
 
 <script>
-
+import axios from "axios";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
 
-data(){
+  data() {
+    return {
+      searchArtist: "",
+      searchLocation: "",
+      searchTitle: "",
+      artworks: [],
+      selectedArtwork: null,
+      isLoading: false
+    };
+  },
 
-return {
-  searchArtist: '',
-  searchLocation: '',
-  searchTitle: '',
-  artworks: {},
-  selectedArtwork: null,
+  methods: {
+    async searchByArtist() {
+      this.artworks = await axios({
+        url: `collection/v1/search`,
+        method: "GET",
+        params: {
+          artistOrCulture: true,
+          q: this.searchArtist,
+        },
+      })
+        .then((response) => {
+          const objectIDs = response.data.objectIDs;
+          this.fetchArtworksDetails(objectIDs);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async searchByLocation() {
+      this.artworks = await axios({
+        url: `collection/v1/search`,
+        method: "GET",
+        params: {
+          geoLocation: true,
+          q: this.searchLocation,
+        },
+      })
+        .then((response) => {
+          const objectIDs = response.data.objectIDs;
+          this.fetchArtworksDetails(objectIDs);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async searchByTitle() {
+      this.artworks = await axios({
+        url: `collection/v1/search`,
+        method: "GET",
+        params: {
+          title: true,
+          q: this.searchTitle,
+        },
+      })
+        .then((response) => {
+          const objectIDs = response.data.objectIDs;
+          this.fetchArtworksDetails(objectIDs);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    fetchArtworksDetails(objectIDs) {
+      this.isLoading = true;
+      const promises = objectIDs.map((objectID) => {
+        return `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`;
+      });
+      Promise.allSettled(promises.map((endpoint) => axios.get(endpoint)))
+        .then((responses) => {
+          const artworks = [];
+          responses.forEach((response) => {
+            if (response.value?.data) {
+              artworks.push(response.value?.data);
+            }
+          });
+          this.artworks = artworks;
+          console.log(artworks);
+        })
+        .catch((error) => {
+          console.log(error);
+        }).finally(() => {
+      this.isLoading = false; // Reseta isLoading após a conclusão da requisição
+    });
+    },
+  },
 };
-
-},
-
-methods:{
-  searchByArtist() {
-      // Fazer a chamada à API passando this.searchArtist como parâmetro de busca
-      // Atualizar this.artworks com os resultados da busca
-    },
-    searchByLocation() {
-      // Fazer a chamada à API passando this.searchLocation como parâmetro de busca
-      // Atualizar this.artworks com os resultados da busca
-    },
-    searchByTitle() {
-      // Fazer a chamada à API passando this.searchTitle como parâmetro de busca
-      // Atualizar this.artworks com os resultados da busca
-    }
-}
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
